afa0e007a51827304a7bd1ed102a391b47da8846
Huang <110813385+lfcypo@users.noreply.github.com>
2024-04-04 22:21:17 +0800

    :sparkles:新增Static静态日志功能

--- ceasylog/ceasylog/setup.py
+++ ceasylog/ceasylog/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ceasylog',
-    version='1.2.1',
+    version='1.2.2',
     keywords='ceasylog',
     description='A great log util from CandyStar',
     author='CandyStar@HuangXudong',
